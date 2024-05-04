6d6d325a71ec4e14d8b32556689a901c51a00e21
Julien <julien.etelain@outlook.com>
2024-05-04 16:20:18 +0200

    Remove unused usings

--- MapToolkit.Test/DataCells/DemDataCellPixelIsPointTest.cs
+++ MapToolkit.Test/DataCells/DemDataCellPixelIsPointTest.cs
@@ -1,9 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows.Markup;
 using MapToolkit.DataCells;
 
 namespace MapToolkit.Test.DataCells
