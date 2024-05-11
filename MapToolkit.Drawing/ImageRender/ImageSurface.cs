47f24bcbfdc18a0ea7ce58543495b65e5ed2bd95
Julien <julien.etelain@outlook.com>
2024-05-11 15:13:37 +0200

    Fix imagesharp render

--- MapToolkit.Drawing/ImageRender/ImageSurface.cs
+++ MapToolkit.Drawing/ImageRender/ImageSurface.cs
@@ -163,9 +163,9 @@ namespace MapToolkit.Drawing.ImageRender
 
             var path = new PathBuilder()
                 .AddArc((float)topLeft.X + radius, (float)topLeft.Y + radius, radius, radius, 0, -90, -90)
-                .AddArc((float)bottomRight.X - radius, (float)topLeft.Y + radius, radius, radius, 0, 180, -90)
+                .AddArc((float)topLeft.X + radius, (float)bottomRight.Y - radius, radius, radius, 180, 0, -90)
                 .AddArc((float)bottomRight.X - radius, (float)bottomRight.Y - radius, radius, radius, 0, 90, -90)
-                .AddArc((float)topLeft.X + radius, (float)bottomRight.Y - radius, radius, radius, 0, 0, -90)
+                .AddArc((float)bottomRight.X - radius, (float)topLeft.Y + radius, radius, radius, 180, 180, -90)
                 .CloseFigure()
                 .Build();
 
