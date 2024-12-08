b0578fdced1b8a5ab22eee9f445424d20db6110f
Julien <julien.etelain@outlook.com>
2024-12-08 21:01:10 +0100

    Additional tests

--- MapToolkit.Test/DataCells/DemDataCellPixelIsPointTest.cs
+++ MapToolkit.Test/DataCells/DemDataCellPixelIsPointTest.cs
@@ -138,7 +138,7 @@ namespace Pmad.Cartography.Test.DataCells
                 { 4, 7, 2 }
             });
 
-            var subCell = dataCell.Crop(new Coordinates(0, 0), new Coordinates(0.5, 0.5)); 
+            var subCell = dataCell.Crop(new Coordinates(0, 0), new Coordinates(0.5, 0.5));
             Assert.Equal(new Coordinates(0, 0), subCell.Start);
             Assert.Equal(new Coordinates(0.5, 0.5), subCell.End);
             Assert.Equal(0.5, subCell.SizeLon);
@@ -161,6 +161,160 @@ namespace Pmad.Cartography.Test.DataCells
             Assert.Equal(4, subCell.Data[0, 1]);
             Assert.Equal(7, subCell.Data[1, 0]);
             Assert.Equal(2, subCell.Data[1, 1]);
+        }
+
+        [Fact]
+        public void Constructor_WithStartEndCoordinates()
+        {
+            var data = new short[3, 3] {
+                { 6, 3, 0 },
+                { 5, 8, 4 },
+                { 4, 7, 2 }
+            };
+            var dataCell = new DemDataCellPixelIsPoint<short>(new Coordinates(0, 0), new Coordinates(1, 1), data);
+
+            Assert.Equal(0, dataCell.Start.Latitude);
+            Assert.Equal(0, dataCell.Start.Longitude);
+            Assert.Equal(1, dataCell.End.Latitude);
+            Assert.Equal(1, dataCell.End.Longitude);
+            Assert.Equal(3, dataCell.PointsLat);
+            Assert.Equal(3, dataCell.PointsLon);
+            Assert.Equal(0.5, dataCell.PixelSizeLat);
+            Assert.Equal(0.5, dataCell.PixelSizeLon);
+        }
+
+
+        [Fact]
+        public void Constructor_WithStartAndPixelSize()
+        {
+            var data = new short[3, 3] {
+                { 6, 3, 0 },
+                { 5, 8, 4 },
+                { 4, 7, 2 }
+            };
+            var pixelSize = new Vector(0.5, 0.5);
+            var dataCell = new DemDataCellPixelIsPoint<short>(new Coordinates(0, 0), pixelSize, data);
+
+            Assert.Equal(0, dataCell.Start.Latitude);
+            Assert.Equal(0, dataCell.Start.Longitude);
+            Assert.Equal(1, dataCell.End.Latitude);
+            Assert.Equal(1, dataCell.End.Longitude);
+            Assert.Equal(3, dataCell.PointsLat);
+            Assert.Equal(3, dataCell.PointsLon);
+            Assert.Equal(0.5, dataCell.PixelSizeLat);
+            Assert.Equal(0.5, dataCell.PixelSizeLon);
+        }
+
+        [Fact]
+        public void Constructor_WithStartAndPixelSize_NonSquare()
+        {
+            var data = new short[, ] {
+                { 6, 3, 0 },
+                { 4, 7, 2 }
+            };
+            var pixelSize = new Vector(0.5, 0.5);
+            var dataCell = new DemDataCellPixelIsPoint<short>(new Coordinates(0, 0), pixelSize, data);
+
+            Assert.Equal(0, dataCell.Start.Latitude);
+            Assert.Equal(0, dataCell.Start.Longitude);
+            Assert.Equal(0.5, dataCell.End.Latitude);
+            Assert.Equal(1, dataCell.End.Longitude);
+            Assert.Equal(2, dataCell.PointsLat);
+            Assert.Equal(3, dataCell.PointsLon);
+            Assert.Equal(0.5, dataCell.PixelSizeLat);
+            Assert.Equal(0.5, dataCell.PixelSizeLon);
+        }
+
+        [Fact]
+        public void ConvertTo()
+        {
+            var data = new short[3, 3] {
+                { 6, 3, 0 },
+                { 5, 8, 4 },
+                { 4, 7, 2 }
+            };
+            var dataCell = new DemDataCellPixelIsPoint<short>(new Coordinates(0, 0), new Coordinates(1, 1), data);
+            var convertedDataCell = dataCell.ConvertTo<int>();
+
+            Assert.Equal(dataCell.Start, convertedDataCell.Start);
+            Assert.Equal(dataCell.End, convertedDataCell.End);
+            Assert.Equal(dataCell.PointsLat, convertedDataCell.PointsLat);
+            Assert.Equal(dataCell.PointsLon, convertedDataCell.PointsLon);
+            Assert.Equal(dataCell.PixelSizeLat, convertedDataCell.PixelSizeLat);
+            Assert.Equal(dataCell.PixelSizeLon, convertedDataCell.PixelSizeLon);
+        }
+
+        [Fact]
+        public void AsPixelIsArea()
+        {
+            var data = new short[3, 3] {
+                { 6, 3, 0 },
+                { 5, 8, 4 },
+                { 4, 7, 2 }
+            };
+            var dataCell = new DemDataCellPixelIsPoint<short>(new Coordinates(0, 0), new Coordinates(1, 1), data);
+            var areaDataCell = dataCell.AsPixelIsArea();
+
+            Assert.Equal(new Coordinates(-0.25, -0.25), areaDataCell.Start);
+            Assert.Equal(new Coordinates(1.25, 1.25), areaDataCell.End);
+            Assert.Equal(dataCell.PointsLat, areaDataCell.PointsLat);
+            Assert.Equal(dataCell.PointsLon, areaDataCell.PointsLon);
+        }
+
+        [Fact]
+        public void AsPixelIsPoint()
+        {
+            var data = new short[3, 3] {
+                { 6, 3, 0 },
+                { 5, 8, 4 },
+                { 4, 7, 2 }
+            };
+            var dataCell = new DemDataCellPixelIsPoint<short>(new Coordinates(0, 0), new Coordinates(1, 1), data);
+
+            Assert.Same(dataCell, dataCell.AsPixelIsPoint());
+        }
+
+        [Fact]
+        public void IsLocal()
+        {
+            var data = new short[3, 3] {
+                { 6, 3, 0 },
+                { 5, 8, 4 },
+                { 4, 7, 2 }
+            };
+            var dataCell = new DemDataCellPixelIsPoint<short>(new Coordinates(0, 0), new Coordinates(1, 1), data);
+
+            Assert.True(dataCell.IsLocal(new Coordinates(0.5, 0.5)));
+            Assert.False(dataCell.IsLocal(new Coordinates(1.5, 1.5)));
+        }
+
+        [Fact]
+        public void Downsample()
+        {
+            var data = new short[5, 5] {
+                { 1, 2, 3, 4, 5 },
+                { 6, 7, 8, 9, 10 },
+                { 11, 12, 13, 14, 15 },
+                { 16, 17, 18, 19, 20 },
+                { 21, 22, 23, 24, 25 }
+            };
+            var dataCell = new DemDataCellPixelIsPoint<short>(new Coordinates(0, 0), new Coordinates(1, 1), data);
+            var downsampledDataCell = dataCell.Downsample(2, null, null, null);
+
+            Assert.Equal(3, downsampledDataCell.PointsLat);
+            Assert.Equal(3, downsampledDataCell.PointsLon);
+
+            Assert.Equal(4, downsampledDataCell.Data[0, 0]);
+            Assert.Equal(6, downsampledDataCell.Data[0, 1]);
+            Assert.Equal(5, downsampledDataCell.Data[0, 2]);
+
+            Assert.Equal(14, downsampledDataCell.Data[1, 0]);
+            Assert.Equal(16, downsampledDataCell.Data[1, 1]);
+            Assert.Equal(20, downsampledDataCell.Data[1, 2]);
+
+            Assert.Equal(12, downsampledDataCell.Data[2, 0]);
+            Assert.Equal(15, downsampledDataCell.Data[2, 1]);
+            Assert.Equal(25, downsampledDataCell.Data[2, 2]);
         }
 
         [Fact]
