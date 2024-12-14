03ab29945f1df167aeb6bd2c899066f875c8ad76
Julien <julien.etelain@outlook.com>
2024-12-14 11:11:49 +0100

    Remove Clipper v1

--- MapToolkit/GeometryExtensions.cs
+++ MapToolkit/GeometryExtensions.cs
@@ -1,10 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Numerics;
-using ClipperLib;
 using GeoJSON.Text.Geometry;
-using Pmad.Geometry;
 
 namespace Pmad.Cartography
 {
